8a86d2c31b1cb7ad9960f804215365733efdaced
Jonathan Holt <jonathan.a.holt@gmail.com>
2020-03-04 00:46:00 +0000

    serve

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const playwright = require('playwright');
 const looksSame = require('looks-same');
 const combineImage = require('merge-img');
+const sharp = require('sharp');
+const tcpPortUsed = require('tcp-port-used');
 const AWS = require('aws-sdk');
 const github = require('@actions/github');
 const core = require('@actions/core');
@@ -16,10 +18,9 @@ const {
   getChromePath
 } = require('./lib');
 
-const VARIABLE_URL = `file://${process.env.GITHUB_WORKSPACE}${core.getInput('storybook_iframe')}`;
+const VARIABLE_URL = core.getInput('storybook_iframe');
 const CONSTANT_URL = core.getInput('constant_url');
-console.log(VARIABLE_URL, CONSTANT_URL);
-const build = core.getInput('storybook_build');
+const serve = core.getInput('storybook_serve');
 const github_token = core.getInput('github_token');
 const s3_access_token = core.getInput('S3_access_token');
 const s3_secret_token = core.getInput('S3_secret_token');
@@ -91,7 +92,6 @@ const checkStory = async (arrDetails, context) => {
               resolve();
           });
         } else {
-          // console.log(name, 'the same');
           resolve();
         }
       });
@@ -112,7 +112,9 @@ const addIssueComment = (image) => {
 };
 
 (async () => {
-  await exec.exec(build);
+  exec.exec(serve);
+  await tcpPortUsed.waitUntilUsed(9001, 1000, 60000).catch((e) => core.setFailed(e));
+
   const opts = ['--no-sandbox', '--disable-setuid-sandbox'];
   const browser = await playwright.chromium.launch({
     opts,
@@ -134,12 +136,17 @@ const addIssueComment = (image) => {
       const filename = `${commit}.png`;
 
       img.write(filename, async () => {
+        await sharp(filename)
+        .resize(800)
+        .webp({ lossless: true })
+        .toFile(filename);
+
         const file = fs.createReadStream(filename);
         const params = {
           Key: filename,
           Body: file,
           Bucket: s3_bucket,
-          ContentType: 'image/png',
+          ContentType: 'image/webp',
           ACL: 'public-read'
         };
 
@@ -148,7 +155,6 @@ const addIssueComment = (image) => {
             console.log('error', error);
             core.setFailed(error);
           }
-          addIssueComment(image.Location);
         });
       });
     });
